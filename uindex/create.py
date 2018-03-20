b80b32dcb879178f51dee895923778c0762f1979
Mike Boers <mikeb@loftysky.com>
2018-03-20 15:48:44 -0400

    Major mmindex refactor to support updating indexes.

--- uindex/create.py
+++ uindex/create.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 from Queue import Queue, Empty
 import argparse
 import datetime
 import functools
 import hashlib
 import itertools
+import json
 import os
 import re
 import sys
 import threading
+from uuid import uuid4
 
+from .utils import iter_raw_index
 
 
 def resumeable_walk(dir_, start=None):
@@ -59,40 +64,10 @@ def _resumeable_walk(dir_, start):
             yield x
 
 
-def _iter_file_paths(root, start, path_excludes, name_excludes):
 
-    for dir_path, dir_names, file_names in resumeable_walk(root, start):
-
-        if path_excludes or name_excludes:
-            for names in (dir_names, file_names):
-
-                i = 0
-                while i < len(names):
-
-                    name = names[i]
-                    exclude = False
-
-                    if name_excludes and any(r.match(name) for r in name_excludes):
-                        exclude = True
-
-                    elif path_excludes:
-                        rel_path = os.path.relpath(os.path.join(dir_path, name), root)
-                        if any(r.match(rel_path) for r in path_excludes):
-                            exclude = True
-
-                    if exclude:
-                        names.pop(i)
-                    else:
-                        i += 1
-
-        for name in file_names:
-            path = os.path.join(dir_path, name)
-            yield path
-
-
-def _checksum_path(path):
+def _checksum_path(to_index):
     hasher = hashlib.sha256()
-    with open(path, 'rb') as fh:
+    with open(to_index[0], 'rb') as fh:
         while True:
             try:
                 chunk = fh.read(65536)
@@ -101,11 +76,11 @@ def _checksum_path(path):
                 # throw an error no matter what you do.
                 if e.errno != 1:
                     raise
-                return path, None
+                return to_index, None
             if not chunk:
                 break
             hasher.update(chunk)
-    return path, hasher.hexdigest()
+    return to_index, hasher.hexdigest()
 
 
 def _threaded_map(num_threads, func, *args_iters, **kwargs):
@@ -188,77 +163,242 @@ def _threaded_map_target(work_queue, result_queue, func):
 
 
 
+class Indexer(object):
+
+    def __init__(self, path_to_index, root=None, start=None, excludes=(),
+        include_dotfiles=False, verbosity=0):
+
+        self.path_to_index = os.path.abspath(path_to_index)
+        self.root = os.path.abspath(root or self.path_to_index)
+        self.start = start
+        self.verbosity = int(verbosity)
+
+        self.name_excludes = []
+        self.path_excludes = []
+
+        for raw in excludes:
+            if raw.startswith('/'):
+                self.path_excludes.append(re.compile(r'^%s$' % raw.strip('/')))
+            else:
+                self.name_excludes.append(re.compile(r'^%s$' % raw.strip('/')))
+        if not include_dotfiles:
+            self.name_excludes.append(re.compile(r'^\.'))
+
+        self.existing = {}
+
+    def auto_start(self, index_path):
+        with open(index_path, 'rb') as out:
+            out.seek(-1000, 2)
+            last = out.read().splitlines()[-1]
+            rel_start = last.split('\t')[-1]
+            self.start = os.path.join(self.root, rel_start)
+
+    def load_existing(self, input_):
+        if isinstance(input_, basestring):
+            input_ = open(input_, 'rb')
+        for entry in iter_raw_index(input_):
+            self.existing[entry.path] = entry
+
+
+    def _iter_file_paths(self):
+
+        path_excludes = self.path_excludes
+        name_excludes = self.name_excludes
+        existing = self.existing
+        root = self.root
+
+        added_count = 0
+        added_bytes = 0
+        total_count = 0
+        total_bytes = 0
+
+        for dir_path, dir_names, file_names in resumeable_walk(self.path_to_index, self.start):
+
+            if path_excludes or name_excludes:
+                for names in (dir_names, file_names):
+
+                    i = 0
+                    while i < len(names):
+
+                        name = names[i]
+                        exclude = False
+
+                        if name_excludes and any(r.match(name) for r in name_excludes):
+                            exclude = True
+
+                        elif path_excludes:
+                            rel_path = os.path.relpath(os.path.join(dir_path, name), walk_root)
+                            if any(r.match(rel_path) for r in path_excludes):
+                                exclude = True
+
+                        if exclude:
+                            names.pop(i)
+                        else:
+                            i += 1
+
+            for name in file_names:
+
+                abs_path = os.path.join(dir_path, name)
+                rel_path = os.path.relpath(abs_path, root)
+                st = os.stat(abs_path)
+
+                total_count += 1
+                total_bytes += st.st_size
+
+                entry = existing.get(rel_path)
+                if entry:
+                    if entry.size == st.st_size and int(entry.mtime) == int(st.st_mtime):
+                        if self.verbosity > 1:
+                            printerr("# Skipping unchanged {}".format(rel_path))
+                        continue
+                    elif self.verbosity > 1:
+                        printerr("# Reindexing changed {}".format(rel_path))
+
+                added_count += 1
+                added_bytes += st.st_size
+
+                yield abs_path, rel_path, st
+
+        self.added_count = added_count
+        self.added_bytes = added_bytes
+        self.total_count = total_count
+        self.total_bytes = total_bytes
+
+    def run(self, out, threads=1, sorted=True):
+
+        self.error_count = 0
+
+        uuid = str(uuid4())
+        out.write('#scan-start {}\n'.format(json.dumps(dict(
+            path_to_index=self.path_to_index,
+            root=self.root,
+            start=self.start,
+            started_at=datetime.datetime.utcnow().isoformat('T'),
+            uuid=uuid,
+        ), sort_keys=True)))
+
+        for (abs_path, rel_path, st), checksum in _threaded_map(
+            threads,
+            _checksum_path,
+            self._iter_file_paths(),
+            sorted=sorted,
+        ):
+
+            # Sometimes there are wierd errors.
+            if not checksum:
+                out.write('#scan-error {}\n'.format(json.dumps(dict(path=rel_path))))
+                self.error_count += 1
+                continue
+
+            formatted = '\t'.join(str(x) for x in (
+                checksum,
+                '{:o}'.format(st.st_mode & 0o7777),
+                st.st_size,
+                st.st_uid,
+                st.st_gid,
+                '{:.2f}'.format(st.st_mtime),
+                rel_path,
+            ))
+            if self.verbosity:
+                print(formatted)
+            out.write(formatted + '\n')
+
+        out.write('#scan-end {}\n'.format(json.dumps(dict(
+            added_count=self.added_count,
+            added_bytes=self.added_bytes,
+            total_count=self.total_count,
+            total_bytes=self.total_bytes,
+            error_count=self.error_count,
+            ended_at=datetime.datetime.utcnow().isoformat('T'),
+            uuid=uuid,
+        ), sort_keys=True)))
+
+
+
+
+
+
+
+
+def printerr(*args, **kwargs):
+    kwargs['file'] = sys.stderr
+    print(*args, **kwargs)
+
 def main(argv=None):
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-D', '--dots', action='store_true')
-    parser.add_argument('-e', '--exclude', action='append', default=[])
-    parser.add_argument('-o', '--out')
-    parser.add_argument('-s', '--start', type=os.path.abspath)
-    parser.add_argument('-S', '--auto-start', action='store_true')
-    parser.add_argument('--unsorted', action='store_true', help='Faster, but might break auto-start.')
-    parser.add_argument('-t', '--threads', type=int, default=1)
-    parser.add_argument('-C', '--root', type=os.path.abspath)
-    parser.add_argument('-v', '--verbose', action='store_true')
+    
+    parser.add_argument('-D', '--include-dotfiles', action='store_true',
+        help="Don't exclude files that start with dots.")
+
+    parser.add_argument('-e', '--exclude', action='append', default=[],
+        help="Exclude files that match this regex; can be used multiple times.")
+
+    parser.add_argument('-o', '--out',
+        help="File to write to instead of stdout.")
+
+    parser.add_argument('-s', '--start', type=os.path.abspath,
+        help="A path to re-start indexing from.")
+    parser.add_argument('-S', '--auto-start', action='store_true',
+        help="Automatically re-start at the last element in the index.")
+    parser.add_argument('-u', '--update', action='store_true',
+        help="Update index with files that were missing or changed from last run.")
+
+    parser.add_argument('--unsorted', action='store_true',
+        help="Will lose less work if there is a crash, but --auto-start will skip over any lost work.")
+
+    parser.add_argument('-t', '--threads', type=int, default=1,
+        help="How many threads to run at once.")
+
+    parser.add_argument('-C', '--root', type=os.path.abspath,
+        help="Root from which relative paths will be derived.")
+
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+        help="Print out progress even if redirecting to --out.")
+
     parser.add_argument('path', type=os.path.abspath)
 
     args = parser.parse_args(argv)
 
-    if args.auto_start and args.start:
-        print >> sys.stderr, "--start and --auto-start don't work together."
+    def feedback(*a, **kwargs):
+        if args.verbose:
+            printerr(*a, **kwargs)
+
+    if sum(map(bool, (args.auto_start, args.start, args.update))) > 1:
+        printerr("--start, --auto-start, and --update don't work together.")
         exit(1)
     if args.auto_start and not args.out:
-        print >> sys.stderr, "--auto-start requires --out."
+        printerr("--auto-start requires --out.")
+        exit(2)
+    if args.update and not args.out:
+        printerr("--update requires --out.")
         exit(2)
 
-    if args.root is None:
-        args.root = args.path
+    indexer = Indexer(
+        path_to_index=args.path,
+        root=args.root,
+        excludes=args.exclude,
+        include_dotfiles=args.include_dotfiles,
+        verbosity=args.verbose,
+    )
 
     if args.auto_start:
-        if os.path.exists(args.out):
-            with open(args.out, 'rb') as out:
-                out.seek(-1000, 2)
-                last = out.read().splitlines()[-1]
-                rel_start = last.split('\t')[-1]
-                args.start = os.path.join(args.root, rel_start)
-                if args.verbose:
-                    print 'Restarting at', args.start
-        elif args.verbose:
-            print >> sys.stderr, 'Output does not exist for --auto-start.'
+        if not os.path.exists(args.out):
+            printerr("Output file must exist for --auto-start.")
+            exit(3)
+        indexer.auto_start(args.out)
+        feedback('Restarting at', indexer.start)
 
-    out = open(args.out, 'ab' if (args.start or args.auto_start) else 'wb') if args.out else sys.stdout
+    if args.update:
+        if not os.path.exists(args.out):
+            printerr("Output file must exist for --update.")
+            exit(3)
+        feedback("Reading existing index to update...")
+        indexer.load_existing(args.out)
 
-    path_excludes = []
-    name_excludes = []
-    for raw in args.exclude:
-        if raw.startswith('/'):
-            path_excludes.append(re.compile(r'^%s$' % raw.strip('/')))
-        else:
-            name_excludes.append(re.compile(r'^%s$' % raw.strip('/')))
-    if not args.dots:
-        name_excludes.append(re.compile(r'^\.'))
+    out = open(args.out, 'ab' if (args.start or args.auto_start or args.update) else 'wb') if args.out else sys.stdout
+    indexer.run(out, threads=args.threads, sorted=not args.unsorted)
 
-    for abs_path, checksum in _threaded_map(args.threads, _checksum_path, _iter_file_paths(args.path, args.start, path_excludes, name_excludes), sorted=not args.unsorted):
-
-        # Sometimes there are wierd errors.
-        if not checksum:
-            continue
-
-        rel_path = os.path.relpath(abs_path, args.root)
-        st = os.stat(abs_path)
-
-        formatted = '\t'.join(str(x) for x in (
-            checksum,
-            '{:o}'.format(st.st_mode & 0o7777),
-            st.st_size,
-            st.st_uid,
-            st.st_gid,
-            '{:.2f}'.format(st.st_mtime),
-            rel_path,
-        ))
-        if args.verbose:
-            print formatted
-        out.write(formatted + '\n')
 
 if __name__ == '__main__':
     exit(main())
--- uindex/utils.py
+++ uindex/utils.py
@@ -1,17 +1,19 @@
-import collections
+from __future__ import print_function
 
+import collections
+import sys
 
 Token = collections.namedtuple('Token', ['path', 'checksum', 'perms', 'size', 'uid', 'gid', 'mtime'])
 
 def iter_raw_index(fh):
     for line_i, line in enumerate(fh):
         line = line.strip()
-        if not line:
+        if not line or line[0] == '#':
             continue
         try:
             checksum, perms, size, uid, gid, mtime, path = line.split('\t')
         except ValueError as e:
-            print 'WARNING: Index parse failure at line {}: {}'.format(line_i, e)
+            print('WARNING: Index parse failure at line {}: {}\n\t{!r}'.format(line_i, e, line), file=sys.stderr)
             return
         yield Token(path, checksum, int(perms), int(size), int(uid), int(gid), float(mtime))
 
