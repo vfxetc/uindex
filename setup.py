18fa8d8b1a23067e97499183f30e941e2069cd11
Mike Boers <mikeb@loftysky.com>
2018-04-25 10:06:02 -0400

    Renamed to uindex; Add project metadata.
    Up until this point this was part of Lofty Sky's "mmsystems" repo. History has been
    rewritten so that it is only the index.

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+
+setup(
+    name='uindex',
+    version='0.1.dev0',
+    description="Micro file index, with diff and dedup tools.",
+    url='https://github.com/vfxetc/uindex',
+    
+    packages=find_packages(exclude=['build*', 'tests*']),
+    include_package_data=True,
+    
+    author='Mike Boers',
+    author_email='floss+uindex@vfxetc.com',
+    license='BSD-3',
+    
+    entry_points={
+        'console_scripts': '''
+            uindex-create = uindex.index.create:main
+            uindex-dedup = uindex.index.dedup:main
+            uindex-diff = uindex.index.diff:main
+        ''',
+    },
+
+    classifiers=[
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Archiving',
+        'Topic :: System :: Filesystems',
+    ],
+    
+)
